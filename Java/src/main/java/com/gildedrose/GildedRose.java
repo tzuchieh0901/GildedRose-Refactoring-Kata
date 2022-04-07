3aa87bb3031791052912ea74d5eb1e9e41269385
tzuchieh.hsieh <red197007145@gmail.com>
2022-04-07 21:28:47 +0800

    抽取方法 increaseQuality

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -10,24 +10,16 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
             if (item.name.equals("Aged Brie")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+                increaseQuality(item);
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                increaseQuality(item);
 
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
+                if (item.sellIn < 11) {
+                    increaseQuality(item);
+                }
 
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
+                if (item.sellIn < 6) {
+                    increaseQuality(item);
                 }
             } else {
                 if (item.quality > 0) {
@@ -44,9 +36,7 @@ class GildedRose {
 
             if (item.sellIn < 0) {
                 if (item.name.equals("Aged Brie")) {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
+                    increaseQuality(item);
                 } else {
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         item.quality = item.quality - item.quality;
@@ -60,6 +50,12 @@ class GildedRose {
                     }
                 }
             }
+        }
+    }
+
+    protected void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
         }
     }
 }
