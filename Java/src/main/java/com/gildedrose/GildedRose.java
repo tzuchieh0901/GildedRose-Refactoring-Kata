58b5200dc7434907196e36007bbd952d4b6a9f5b
tzuchieh.hsieh <red197007145@gmail.com>
2022-04-07 21:57:21 +0800

    搬移 item.sellIn < 0 的各種判斷邏輯

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -16,6 +16,9 @@ class GildedRose {
             item.sellIn = item.sellIn - 1;
             if (item.name.equals("Aged Brie")) {
                 increaseQuality(item);
+                if (item.sellIn < 0) {
+                    increaseQuality(item);
+                }
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 increaseQuality(item);
 
@@ -26,18 +29,25 @@ class GildedRose {
                 if (item.sellIn < 5) {
                     increaseQuality(item);
                 }
+
+                if (item.sellIn < 0) {
+                    item.quality = item.quality - item.quality;
+                }
             } else {
                 decreaseQuality(item);
+                if (item.sellIn < 0) {
+                    decreaseQuality(item);
+                }
             }
 
             if (item.sellIn < 0) {
                 if (item.name.equals("Aged Brie")) {
-                    increaseQuality(item);
+//                    increaseQuality(item);
                 } else {
                     if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        item.quality = item.quality - item.quality;
+//                        item.quality = item.quality - item.quality;
                     } else {
-                        decreaseQuality(item);
+//                        decreaseQuality(item);
                     }
                 }
             }
