75a75c7d5e3d7e5ec83f95baf5e7f134d9aa6629
tzuchieh.hsieh <red197007145@gmail.com>
2022-04-07 21:53:59 +0800

    slide statement 讓主要邏輯緊密靠近

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -13,23 +13,22 @@ class GildedRose {
                 continue;
             }
 
+            item.sellIn = item.sellIn - 1;
             if (item.name.equals("Aged Brie")) {
                 increaseQuality(item);
             } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 increaseQuality(item);
 
-                if (item.sellIn < 11) {
+                if (item.sellIn < 10) {
                     increaseQuality(item);
                 }
 
-                if (item.sellIn < 6) {
+                if (item.sellIn < 5) {
                     increaseQuality(item);
                 }
             } else {
                 decreaseQuality(item);
             }
-
-            item.sellIn = item.sellIn - 1;
 
             if (item.sellIn < 0) {
                 if (item.name.equals("Aged Brie")) {
